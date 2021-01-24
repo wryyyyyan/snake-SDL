52875764e5d95ef87f4225ba5233c8ccfb60b0f0
Yan Alcantara <yan_sami@hotmail.com>
2021-01-24 10:44:24 -0300

    inicializa subsistema de video e cria a janela

---
+++ libs/render.h
@@ -0,0 +1,24 @@
+#include <SDL2/SDL.h>
+
+#ifndef RENDER_H
+#define RENDER_H
+
+int init_render(void) {
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window * window;
+	SDL_Renderer * renderer;
+	
+	window = SDL_CreateWindow(
+			"Snake",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			320,
+			320,
+			SDL_WINDOW_OPENGL);
+
+	return 0;
+}
+
+
+#endif
