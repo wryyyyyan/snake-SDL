84434c4b5235720fd389bb205f5cf5625fac6af9
Yan Alcantara <yan_sami@hotmail.com>
2021-01-26 13:06:56 -0300

    função create_snake adicioanda

--- libs/snake.h
+++ libs/snake.h
@@ -11,4 +11,18 @@ typedef struct Snake {
 	SDL_Point * body;
 } Snake;
 
+
+Snake create_snake(SDL_Color color) {
+	Snake snake;
+	snake.color = color;
+	snake.sprite.x = 0;
+	snake.sprite.y = 0;
+	snake.sprite.w = snake.sprite.h = 16;
+	snake.body_capacity = 10;
+	snake.body_size = 0;
+	snake.body = calloc(snake.body_capacity, sizeof(snake.body));
+
+	return snake;
+}
+
 #endif
