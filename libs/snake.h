acabd4a430fa9e2d8060ff388bfbc93724fec5e0
Yan Alcantara <yan_sami@hotmail.com>
2021-01-28 10:34:22 -0300

    função move_snake adicionada move o primeiro segmento da cobra (cabeça) em uma das 4 direções (cima, esquerda, baixo e direita) e depois itera pelos segmentos, do ultimo até o segundo, igualando o segmento atual ao anterior, até que todos tenham seguido o movimento da "cabeça"

--- libs/snake.h
+++ libs/snake.h
@@ -57,6 +57,31 @@ void change_direction(SDL_Event * keypress, Snake * snake) {
 	}
 }
 
+void move_snake(Snake * snake) {
+	SDL_Point velocity = { 0, 0 };
+	switch(snake->current_direction) {
+		case UP:
+			velocity.x = 0, velocity.y = -16;
+			break;
+		case LEFT:
+			velocity.x = -16, velocity.y =  0;
+			break;
+		case DOWN:
+			velocity.x = 0, velocity.y = 16;
+			break;
+		case RIGHT:
+			velocity.x = 16, velocity.y = 0;
+			break;
+	}
+
+
+	snake->body[0].x += velocity.x;
+	snake->body[0].y += velocity.y;
+	for(int i = snake->body_size - 1; i > 0; i--) {
+		snake->body[i] = snake->body[i - 1];
+	}
+}
+
 void render_snake(SDL_Surface * buffer, Snake * snake) {
 	for(int i = 0; i <= snake->body_size - 1; i++) {
 		snake->sprite.x = snake->body[i].x;
