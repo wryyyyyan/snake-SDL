1b04cd25004785c0ce6ab2c3feb3e10f8d49bc9e
Yan Alcantara <yan_sami@hotmail.com>
2021-02-02 01:55:45 -0300

    função para criar uma nova fruta adicionada

--- libs/fruit.h
+++ libs/fruit.h
@@ -9,6 +9,16 @@ typedef struct Fruit {
 	SDL_Rect sprite;
 } Fruit;
 
+Fruit create_fruit(SDL_Color color, SDL_Point first_position) {
+	Fruit fruit;
+	fruit.position = first_position;
+	fruit.color = color;
+	fruit.sprite.x = fruit.position.x;
+	fruit.sprite.y = fruit.position.y;
+	fruit.sprite.w = fruit.sprite.h = 16;
+
+	return fruit;
+}
 
 
 #endif
