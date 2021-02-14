e7a260ec9240a1706e0ba38bcfa1a798f9b4619c
Yan Alcantara <yan_sami@hotmail.com>
2021-02-14 15:40:21 -0300

    inicializa API do SDL_mixer

--- libs/audio.h
+++ libs/audio.h
@@ -3,4 +3,12 @@
 #ifndef AUDIO_H
 #define AUDIO_H
 
+int init_audio() {
+	if(Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 1024) < 0) {
+		printf("erro inicializando SDL_Mixer: %s\n", Mix_GetError());
+		return 1;
+	}
+	return 0;
+}
+
 #endif
