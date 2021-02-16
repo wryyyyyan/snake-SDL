99b72ce82e4d8442fbf952d8bac12d34df1b133e
Yan Alcantara <yan_sami@hotmail.com>
2021-02-16 01:31:42 -0300

    constantes adicionadas

--- libs/common.h
+++ libs/common.h
@@ -1,8 +1,15 @@
+#include <SDL2/SDL.h>
 
 #ifndef COMMON_H
 #define COMMON_H
 
+const int screen_width = 1080;
+const int screen_height = 520;
+const int tile_size = 16;
 
+const SDL_Color white = { 255, 255, 255, 255};
+const SDL_Color red = { 255, 0, 0, 255 };
 
+const int delay = 1000 / 15;
 
 #endif
