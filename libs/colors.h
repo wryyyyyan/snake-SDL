867938e5e011a1449bcc46dcb3f435a73a63bd04
Yan Alcantara <yan_sami@hotmail.com>
2021-03-04 13:32:59 -0300

    arquivo de cabeçalho com declaração das cores

---
+++ libs/colors.h
@@ -0,0 +1,11 @@
+#include <SDL2/SDL.h>
+
+#ifndef COLORS_H
+#define COLORS_H
+
+extern const SDL_Color white       ;
+extern const SDL_Color red         ;
+extern const SDL_Color light_green ;
+extern const SDL_Color grey        ;
+
+#endif
